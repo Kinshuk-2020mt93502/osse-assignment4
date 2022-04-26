b1ec4ac1ed9c375386bc2d7bb0077c731284e402
Kinshuk Lahiri <2020mt93502@wilp.bits-pilani.ac.in>
2022-04-27 00:00:04 +0530

    Student Route added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 import Homepage from "./component/Homepage";
 import Student2020MT93502 from "./component/Student2020MT93502";
+import Student2020MT93501 from "./component/Student2020MT93501";
 
 function App() {
   return (
