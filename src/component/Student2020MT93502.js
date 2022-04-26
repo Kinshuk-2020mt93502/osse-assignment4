d628e1738389b035ca12a5d38e06fcad2d9b4543
Kinshuk Lahiri <kinshuk.lahiri@hcah.in>
2022-04-26 18:02:17 +0530

    Code Deployed and github pages submitted

--- src/component/Student2020MT93502.js
+++ src/component/Student2020MT93502.js
@@ -3,7 +3,18 @@ import StudentBio from "./StudentBio";
 class Student2020MT93502 extends React.Component {
 
     render() {
-        return (<StudentBio name="Kinshuk Lahiri" />)
+        return (<StudentBio
+            name="Kinshuk Lahiri"
+            location="Mohali, Punjab"
+            githuburl="https://github.com/Kinshuk-2020mt93502/osse-assignment4"
+            designation="Engineering Manager"
+            repository_status="Repository Owner"
+            github="Kinshuk-2020MT93502"
+            linkedin="kinshuklahiri"
+            email="2020MT93502@wilp.bits-pilani.ac.in"
+            image="2020MT93502.jpg"
+
+        />)
     }
 }
 
--- src/component/StudentBio.js
+++ src/component/StudentBio.js
@@ -24,11 +24,11 @@ class StudentBio extends React.Component {
                             <div className="card">
                                 <div className="card-body">
                                     <div className="d-flex flex-column align-items-center text-center">
-                                        <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width={150} />
+                                        <img src={require('../studentImage/'+this.props.image)} alt={this.props.name} className="rounded-circle" width={150} />
                                         <div className="mt-3">
                                             <h4>{this.props.name}</h4>
                                             <p className="text-secondary mb-1">{this.props.designation}</p>
-                                            <p className="text-muted font-size-sm">{this.location}</p>
+                                            <p className="text-muted font-size-sm">{this.props.location}</p>
                                         </div>
                                     </div>
                                 </div>
@@ -39,12 +39,12 @@ class StudentBio extends React.Component {
                                     <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                                         <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-github mr-2 icon-inline"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" /></svg>Github</h6>
                                         <span className="text-secondary">
-                                            <a href="https://github.com/Kinshuk-2020mt93502" target="_blank" style={this.buttonStyle}>Kinshuk-2020mt93502</a></span>
+                                            <a href={"https://github.com/" + this.props.github} target="_blank" style={this.buttonStyle}>{this.props.github}</a></span>
                                     </li>
                                     <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                                         <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg> Linkedin</h6>
                                         <span className="text-secondary">
-                                            <a href="https://www.linkedin.com/in/kinshuklahiri/" style={this.buttonStyle} target="_blank">@kinshuklahiri</a></span>
+                                            <a href={"https://www.linkedin.com/in/" + this.props.linkedin} style={this.buttonStyle} target="_blank">@{this.props.linkedin}</a></span>
                                     </li>
                                 </ul>
                             </div>
@@ -57,7 +57,7 @@ class StudentBio extends React.Component {
                                             <h6 className="mb-0">Full Name</h6>
                                         </div>
                                         <div className="col-sm-9 text-secondary">
-                                            Kinshuk Lahiri
+                                            {this.props.name}
                                         </div>
                                     </div>
                                     <hr />
@@ -66,7 +66,7 @@ class StudentBio extends React.Component {
                                             <h6 className="mb-0">Role</h6>
                                         </div>
                                         <div className="col-sm-9 text-secondary">
-                                            Repository Owner
+                                            {this.props.repository_status}
                                         </div>
                                     </div>
                                     <hr />
@@ -75,7 +75,7 @@ class StudentBio extends React.Component {
                                             <h6 className="mb-0">Email</h6>
                                         </div>
                                         <div className="col-sm-9 text-secondary">
-                                            2020MT93502@wilp.bits-pilani.ac.in
+                                            {this.props.email}
                                         </div>
                                     </div>
                                     <hr />
@@ -93,7 +93,7 @@ class StudentBio extends React.Component {
                                             <h6 className="mb-0">Designation</h6>
                                         </div>
                                         <div className="col-sm-9 text-secondary">
-                                            Engineering Manager
+                                            {this.props.designation}
                                         </div>
                                     </div>
                                     <hr />
@@ -122,7 +122,7 @@ class StudentBio extends React.Component {
                                             <h6 className="mb-0">Project Repository</h6>
                                         </div>
                                         <div className="col-sm-9 text-secondary">
-                                            <a className="btn btn-blue" href="https://github.com/Kinshuk-2020mt93502/osse-assignment4" target="_blank">
+                                            <a className="btn btn-blue" href={this.props.githuburl} target="_blank">
                                                 View
                                             </a>
                                         </div>
@@ -134,7 +134,7 @@ class StudentBio extends React.Component {
                                             <h6 className="mb-0">Address</h6>
                                         </div>
                                         <div className="col-sm-9 text-secondary">
-                                            Mohali, Punjab
+                                            {this.props.location}
                                         </div>
                                     </div>
                                 </div>
